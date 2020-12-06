df636607c676852d4d7d1b33201bad81e6f311db
Abhishek <abjaiswal1997@gmail.com>
2020-12-06 20:37:25 +0530

    Final Solution

--- src/EdubriteQuestion/Query.java
+++ src/EdubriteQuestion/Query.java
@@ -1,6 +1,8 @@
 package EdubriteQuestion;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.io.*;
+
 public class Query {
 	
 	//function to print employee list (with name, dept, region) sorted by emp name, asc and desc
@@ -34,7 +36,8 @@ public class Query {
 		}
 		
 		Collections.sort(empName);
-		System.out.println("In ascending order");
+		System.out.println("Employee List in ascending order");
+		System.out.println();
 		
 		for (String name : empName) {
 			String dId = employeeName_dId.get(name);
@@ -44,7 +47,9 @@ public class Query {
 			System.out.println(name + " " + deptName + " " + region);
 		}
 		
-		System.out.println("In descending order");
+		System.out.println();
+		System.out.println("Employee List in descending order");
+		System.out.println();
 		System.out.println("EmpName"+" "+"DeptName"+" "+"RegionName");
 		Collections.sort(empName,Collections.reverseOrder());
 		for (String name : empName) {
@@ -60,28 +65,54 @@ public class Query {
 
 	//function to print Emp count in each department and print which dept has no employees
 	
-	public static void secondQuery(List<ArrayList<String>> departmentList, List<ArrayList<String>> employeeList){
+	public static void secondQuery(List<ArrayList<String>> departmentList, List<ArrayList<String>> employeeList,List<ArrayList<String>> regionList){
 		
 		Map<String, Long> employeePerDepartment = new HashMap<>();
+		
+		System.out.println();
 		System.out.println("Employee Count in each department");
+		System.out.println();
+		String noEmployee=" ";
 		for(ArrayList<String> i:departmentList){
+			
 			String dId = i.get(0);
+			String deptName = i.get(1);
 			long count  = employeeList.stream().filter(o -> o.get(2).contentEquals(dId)).count();
 			employeePerDepartment.put(dId, count);
 			
 			if(!dId.equals("Dept Id")){
-				System.out.println(dId+" "+count);
-			}	
+				System.out.println(deptName+" "+count);
+			}
+			if(count == 0){
+				noEmployee = deptName;
+			}
 		}
 		
+		System.out.println();
 		System.out.println("Dept which has no Employees");
+		System.out.println();
 		for(String dId : employeePerDepartment.keySet()) {
 			if(employeePerDepartment.get(dId) == 0)
-				System.out.println(dId);
+				System.out.println("Id: "+dId+" Dept Name: "+noEmployee);
+		}
+		
+		System.out.println();
+		System.out.println("Employee count in each region");
+		System.out.println();
+		
+		for(ArrayList<String> region:regionList){
+			
+			List<List<String>> departmentInCurrentRegion = departmentList.stream().filter(department -> department.get(2).contentEquals(region.get(0))).collect(Collectors.toList());
+
+			long count = departmentInCurrentRegion.stream().mapToLong(o -> employeePerDepartment.get(o.get(0))).sum();
+			
+			if(!region.get(1).equals("RegionName")){
+				System.out.println(region.get(1)+" "+ count);
+			}
+			
+			
 		}
 	}
-	
-	
 	
 	public static void main(String agr[]){
 		
@@ -96,8 +127,8 @@ public class Query {
 		regionList.addAll(region.read(regionFileName));
 		
 		
-//		firstQuery(departmentList,employeeList,regionList);
-		secondQuery(departmentList,employeeList);
+		firstQuery(departmentList,employeeList,regionList);
+		secondQuery(departmentList,employeeList,regionList);
 		
 		
 		
